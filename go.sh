b1b65325a6758c4940e38a03689e8969757950b9
Mushroom Hostage <mushroomhostage@yahoo.com>
2012-01-29 11:16:47 -0800

    Update for 1.1-R2

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 #!/bin/sh -x
-CLASSPATH=../craftbukkit-1.1-R1.jar:../plugins-disabled/OddItem-0.8.1.jar javac FreeTrade.java -Xlint:deprecation -Xlint:unchecked
+CLASSPATH=../craftbukkit-1.1-R2.jar:../plugins-disabled/OddItem-0.8.1.jar javac FreeTrade.java -Xlint:deprecation -Xlint:unchecked
 rm -rf me
 mkdir -p me/exphc/FreeTrade
 mv *.class me/exphc/FreeTrade/
